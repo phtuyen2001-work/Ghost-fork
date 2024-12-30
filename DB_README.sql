c7bd0371ceb16365fcf7cb555100b8d3484de384
Hon-Tuyen Pham <tuyen.hon.pham@aioz.io>
2024-12-30 14:55:10 +0700

    update db_readme.sql

--- DB_README.sql
+++ DB_README.sql
@@ -5,14 +5,9 @@ CREATE TABLE infras (
 	CONSTRAINT uc_slug UNIQUE (slug)
 )
 
-INSERT INTO infras VALUES
-	('00001', 'W3 Storage', 'w3s'),
-	('00002', 'W3 AI', 'w3ai')
-
-
 CREATE TABLE posts_infras (
 	id VARCHAR(24) PRIMARY KEY,
-	post_id VARCHAR(24) NOT NULL,
+	post_id varchar(24) NOT NULL COLLATE utf8mb4_0900_ai_ci,
 	infra_id VARCHAR(24) NOT NULL,
 	sort_order INT UNSIGNED NOT NULL DEFAULT 0,
 	CONSTRAINT fk_postId
@@ -25,3 +20,7 @@ CREATE TABLE posts_infras (
 			ON UPDATE CASCADE
 			ON DELETE CASCADE
 )
+
+INSERT INTO infras VALUES
+	('00001', 'W3 Storage', 'w3s'),
+	('00002', 'W3 AI', 'w3ai')
